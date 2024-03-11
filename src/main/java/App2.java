5d906628530e6ad3760514477876cf2512cdfad2
Yan Balbino <yanbnogueira@hotmail.com>
2024-03-10 23:49:14 -0300

    seriosamente nao aguento mais

--- src/main/java/App2.java
+++ src/main/java/App2.java
@@ -11,15 +11,30 @@ public class App2 {
 
         List<String> transicoes = new ArrayList<>();
 
+        /* 
         transicoes.add(">q0[q0, q1]");
         transicoes.add("q1[q2, q0]");
         transicoes.add("*q2[q1, q2]");
-
+        */
+         
+        transicoes.add(">q0[q0, q1]");
+        transicoes.add("q1[q2, q0]");
+        transicoes.add("q2[q3, q1]");
+        transicoes.add("*q3[q2, q3]");
+        
         System.out.println(transicoes);
 
         System.out.println("Digite a cadeia: ");
         String cadeia = s.nextLine();
-        if (Leitura(transicoes, cadeia) == 1){
+
+        String estadoFinal = "";
+        for (String fin : transicoes){
+            if (fin.contains("*")){
+                estadoFinal = fin.substring(1, 3);
+            }
+        }
+
+        if (Leitura(transicoes, cadeia, estadoFinal) == 1){
             System.out.println("A cadeia foi aceita.");
         }
         else{
@@ -28,7 +43,7 @@ public class App2 {
 
         s.close();
     }
-    public static int Leitura(List<String> transicoes, String cadeia){
+    public static int Leitura(List<String> transicoes, String cadeia, String estadoFinal){
         String estadoAtual = "";
 
         char w0 = cadeia.charAt(0);
@@ -36,9 +51,11 @@ public class App2 {
             if (j.contains(">")){ // leitura inicial da cadeia
                 if (w0 == '0'){
                     estadoAtual = j.substring(4, 5);
+                    break;
                 }
                 if (w0 == '1'){
                     estadoAtual = j.substring(8, 10);
+                    break;
                 }
             }
         }
@@ -47,17 +64,31 @@ public class App2 {
             char w = cadeia.charAt(i);
             
             for (String k : transicoes){
-                if (k.){
+                if (!k.contains(">")){
+                    if ((k.substring(0, 2).equals(estadoAtual))){
+                        if (w == '0'){
+                            estadoAtual = k.substring(3, 5);
+                            break;
+                        }
+                        if (w == '1'){
+                            estadoAtual = k.substring(7, 9);
+                            break;
+                        }
+                    }
+                }
+                if (k.contains("*")){
                     if (w == '0'){
                         estadoAtual = k.substring(4, 6);
+                        break;
                     }
                     if (w == '1'){
                         estadoAtual = k.substring(8, 10);
+                        break;
                     }
                 }
             }
         }
-        if (estadoAtual.contains("*")){
+        if (estadoAtual.equals(estadoFinal)){
             return 1;
         }
         return 0;
